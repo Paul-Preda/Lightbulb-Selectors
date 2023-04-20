e6b1b84bf4ae8af7c635ba1bdc6e784d81c9a237
Paul Preda <ppd1512@gmail.com>
2023-04-20 12:27:16 +0100

    created the lightbulb setup :)

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Write your code here
+const bulb1 = document.querySelector("#lightbulb1")
+const bulb2 = document.querySelector("#lightbulb2")
+const bulb3 = document.querySelector("#lightbulb3")
+const subtitle = document.querySelector(".subtitle")
+
+
+bulb1.addEventListener("mouseover",counting)
+bulb2.addEventListener("mouseover",counting)
+bulb3.addEventListener("mouseover",counting)
+
+let count = 0;
+function counting(){
+    count++
+    console.log(count)
+    subtitle.innerHTML = `You've hovered over the lights ${count} times`
+    this.classList.toggle("active")
+}
